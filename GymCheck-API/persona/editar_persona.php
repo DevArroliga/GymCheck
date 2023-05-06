8bcf966374b5aa7472dc822a3d34887158e22912
josar <josarroliga0943@outlook.es>
2023-05-05 18:36:12 -0600

    correciones en la base de datos y editar persona

---
+++ GymCheck-API/persona/editar_persona.php
@@ -0,0 +1,20 @@
+<?php
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        require_once '../conexion.php';
+
+        $idPersona = $_POST["idPersona"];      
+        $correo = $_POST["correo"];
+
+        $my_query = "UPDATE persona SET correo= '".$correo."' WHERE idC=".$idC;
+        $result = $mysql->query($my_query);
+
+        if($result == true){
+            echo 'Persona editada exitosamente';
+        } else { 
+            echo 'Error';
+        }
+    } else {
+        echo 'Error desconocido';
+    }
+
+?>
