0f21ba92f02b39f0010cac94fc604823c8b9d487
josar <josarroliga0943@outlook.es>
2023-05-08 14:26:31 -0600

    controlador editar persona

--- app/src/main/java/Controladores/PersonaControlador.kt
+++ app/src/main/java/Controladores/PersonaControlador.kt
@@ -4,9 +4,11 @@ import okhttp3.*
 import java.io.IOException
 
 class PersonaControlador {
-    private val urlAPI = "http://192.168.0.15/GymCheck-API/persona/agregar_persona.php"
+    // IP Roberto: "192.168.0.15"
 
     fun agregarPersona(persona: Persona){
+        val urlAPI = "http://192.168.0.15/GymCheck-API/persona/agregar_persona.php"
+
         val requestBody: RequestBody = FormBody.Builder()
             .add("nombre", persona.nombre)
             .add("apellido", persona.apellido)
@@ -19,6 +21,36 @@ class PersonaControlador {
             .post(requestBody)
             .build()
 
+        val client = OkHttpClient()
+
+        client.newCall(request).enqueue(object: Callback{
+            override fun onResponse(call: Call, response: Response) {
+                if (response.isSuccessful){
+                    val respuesta = response.body?.string()
+                    println(respuesta)
+                }
+                else {
+                    println("Error en la respuesta del servidor")
+                }
+            }
+
+            override fun onFailure(call: Call, e: IOException) {
+                println("Error en la petición HTTP: ${e.message}")
+            }
+        })
+    }
+
+    fun editarPersona(idPersona: Int, correo: String){
+        val urlAPI = "http://192.168.0.15/GymCheck-API/persona/editar_persona.php"
+
+        val formBody = FormBody.Builder()
+            .add("idPersona", idPersona.toString())
+            .add("correo", correo)
+            .build()
+        val request = Request.Builder()
+            .url(urlAPI)
+            .post(formBody)
+            .build()
         val client = OkHttpClient()
 
         client.newCall(request).enqueue(object: Callback{
