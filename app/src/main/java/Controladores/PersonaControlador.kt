a86c5bf520b7ab5618951f1edd24d5f7d066140a
josar <josarroliga0943@outlook.es>
2023-05-07 18:45:03 -0600

    creacion controlador persona y funcion agregar

---
+++ app/src/main/java/Controladores/PersonaControlador.kt
@@ -0,0 +1,40 @@
+package Controladores
+import Entidades.Persona
+import okhttp3.*
+import java.io.IOException
+
+class PersonaControlador {
+    private val urlAPI = "http://192.168.73.130/GymCheck-API/persona/agregar_persona.php"
+
+    fun agregarPersona(persona: Persona){
+        val requestBody: RequestBody = FormBody.Builder()
+            .add("nombre", persona.nombre)
+            .add("apellido", persona.apellido)
+            .add("fechaNac", persona.fechaNac)
+            .add("correo", persona.correo)
+            .build()
+
+        val request: Request = Request.Builder()
+            .url(urlAPI)
+            .post(requestBody)
+            .build()
+
+        val client = OkHttpClient()
+
+        client.newCall(request).enqueue(object: Callback{
+            override fun onResponse(call: Call, response: Response) {
+                if (response.isSuccessful){
+                    val respuesta = response.body?.string()
+                    println(request)
+                }
+                else {
+                    println("Error en la respuesta del servidor")
+                }
+            }
+
+            override fun onFailure(call: Call, e: IOException) {
+                println("Error en la petición HTTP: ${e.message}")
+            }
+        })
+    }
+}
