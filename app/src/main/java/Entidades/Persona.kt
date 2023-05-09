448ae9d361c271b88b6037bf634575e821f40082
Xeppyz <allan.silva13589@est.uca.edu.ni>
2023-05-08 18:23:23 -0600

    Cambios en la entidad y controlador

--- app/src/main/java/Entidades/Persona.kt
+++ app/src/main/java/Entidades/Persona.kt
@@ -6,5 +6,6 @@ data class Persona (
     val nombre: String,
     val apellido: String,
     val fechaNac: String, // Tipo de dato String, pero sera insertado con un formato de tipo Date en la base de datos.
-    val correo: String
+    val correo: String,
+    val cedula: String
 )
