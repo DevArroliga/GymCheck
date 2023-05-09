1f0a0aa44e3ff923b130aedac6e62e6a73e60f23
Xeppyz <allan.silva13589@est.uca.edu.ni>
2023-05-08 20:59:07 -0600

    Implementacion de foto en producto y anuncio

--- app/src/main/java/com/example/gymcheck/AgregarAnuncio.kt
+++ app/src/main/java/com/example/gymcheck/AgregarAnuncio.kt
@@ -1,10 +1,17 @@
 package com.example.gymcheck
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.findNavController
 import com.example.gymcheck.databinding.FragmentAgregarAnuncioBinding
 import com.google.android.material.datepicker.MaterialDatePicker
@@ -13,6 +20,13 @@ import java.util.*
 
 class AgregarAnuncio : Fragment() {
 
+
+    val pickMedia = registerForActivityResult(ActivityResultContracts.PickVisualMedia()){uri->
+        if(uri != null){
+            binding.imgAnuncio.setImageURI(uri)
+        }
+
+    }
 private lateinit var binding: FragmentAgregarAnuncioBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,6 +48,10 @@ private lateinit var binding: FragmentAgregarAnuncioBinding
         super.onViewCreated(view, savedInstanceState)
         init()
         showDatePicker()
+
+        binding.imgAnuncio.setOnClickListener {
+            inicioFoto()
+        }
     }
 
 
@@ -58,6 +76,36 @@ private lateinit var binding: FragmentAgregarAnuncioBinding
             }
         }
 
+    }
+    private fun inicioFoto(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            when{
+                ContextCompat.checkSelfPermission(
+                    binding.imgAnuncio.context, Manifest.permission.READ_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED->{
+                    lanzarFoto()
+                }
+                else -> requestPermissionLaucher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+            }
+        }else{
+            lanzarFoto()
+
+        }
+
+
+    }
+    private val requestPermissionLaucher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted) {
+            lanzarFoto()
+        } else {
+            Toast.makeText(context, "Activar los permisos", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun lanzarFoto() {
+        pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
     }
 
 
