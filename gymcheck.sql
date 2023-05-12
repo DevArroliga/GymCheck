37b8097c2d7deeaaf59f4a235ed85573030b9546
josar <josarroliga0943@outlook.es>
2023-05-12 16:54:24 -0600

    longblob img anuncio

--- gymcheck.sql
+++ gymcheck.sql
@@ -35,6 +35,7 @@ CREATE TABLE `anuncio` (
   `tituloAnuncio` varchar(50) NOT NULL,
   `descripcion` varchar(100) NOT NULL,
   `fecha` date NOT NULL,
+  `img` longblob NOT NULL,
   `estado` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
