a9849941e055818adcc4b4216b45f62679769d5b
josar <josarroliga0943@outlook.es>
2023-05-12 01:04:33 -0600

    correcion tipo de dato producto

--- gymcheck.sql
+++ gymcheck.sql
@@ -80,7 +80,7 @@ CREATE TABLE `producto` (
   `descripcion` varchar(100) NOT NULL,
   `precio` float NOT NULL,
   `stock` int(11) NOT NULL,
-  `img` blob NOT NULL,
+  `img` longblob NOT NULL,
   `estado` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
