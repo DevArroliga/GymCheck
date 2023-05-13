d7f5cbea641e3a448b4fac798fa4a5c1ba61bbfe
Xeppyz <allan.silva13589@est.uca.edu.ni>
2023-05-13 01:48:05 -0600

    Cmbio en el mostrar que solo muestre los estados = 1

--- GymCheck-API/anuncio/mostrar_anuncio.php
+++ GymCheck-API/anuncio/mostrar_anuncio.php
@@ -5,7 +5,7 @@ if ($mysql->connect_error) {
     die("Conexion fallida: " . $mysql->connect_error);
 }
 
-$sql = "SELECT idAnuncio, tituloAnuncio, descripcion, fecha, img FROM anuncio";
+$sql = "SELECT idAnuncio, tituloAnuncio, descripcion, fecha, img FROM anuncio WHERE estado = 1";
 $result = $mysql->query($sql);
 
 if ($result->num_rows >= 0) {
